e66d46c1491e34b292b830d82abe995ffa4da4fe
Rizruf <sova_nikeo@mail.ru>
2025-12-09 09:52:12 +0600

    Особенности: Выполнено задание статистика массивов

---
+++ SyntaxPractice/Exam.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SyntaxPractice
+{
+    internal class Exam
+    {
+        public static int[] RndArray()
+        {
+            Random rnd = new Random();
+            int size = rnd.Next(1,10);
+            int[] array = new int[size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 100);
+            }
+            return array;
+        }
+        public static void GetArrayStatistics(int[] inputData, out int max, out int min, out int sum, out decimal avg)
+        {
+            max = inputData[0]; min = inputData[0];
+
+            sum = 0; avg = 0; 
+            int counts = inputData.Length;
+
+            for (int i = 0; i < inputData.Length; i++)
+            {
+                if (inputData[i] > max) max = inputData[i]; //максимальный элемент
+                if (inputData[i] < min) min = inputData[i]; //минимальный элемент
+                sum += inputData[i];
+            }
+            avg = (decimal)sum / (decimal)counts;
+        }
+
+        public static void Test_GetArrayStatistics()
+        {
+            int[] array = RndArray(); // получил рандомно наполненный массив
+            int max = 0, min = 0, sum = 0; decimal avg = 0;
+            GetArrayStatistics(array, out max, out min, out sum, out avg);
+
+            foreach (int number in array)
+            {
+                Console.Write(number + " ");
+            }
+            Console.Write($"\n{max} {min} {sum} " + avg);
+        }
+    }
+}
--- SyntaxPractice/Program.cs
+++ SyntaxPractice/Program.cs
@@ -6,9 +6,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Exam.Test_GetArrayStatistics();
+
             //Block6_ComplicatedTasks.GameGuessNumber2();
             //Block6_ComplicatedTasks.ATMachine2();
-            Block6_ComplicatedTasks.UserOddNumbers();
+            //Block6_ComplicatedTasks.UserOddNumbers();
 
 
             //Block5_ComplicatedTasks.Test_GetMatrixSum();
